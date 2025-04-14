a78bdb03e022c1014ba8b23b7f6a8f244c5be457
Sickle41 <sickle41@gmail.com>
2025-04-14 13:19:10 -0500

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿
+
+
+Console.WriteLine("Plan Your Heist!");
+
+void DisplayMenu()
+{
+    Console.WriteLine(@"Choose an option
+    1. Display Team Members
+    2. Add Team Members
+    3. Exit");
+}
---
+++ TeamMember.cs
@@ -0,0 +1,6 @@
+
+public class TeamMember {
+    public string Name { get; set; }
+    public int SkillLevel { get; set; }
+    public decimal CourageFactor { get; set; }
+}
